987f74271872dea0a14bbee7547513c462676dac
Greggory Rothmeier <greggroth@gmail.com>
2012-03-09 12:19:32 -0500

    added post summary to front page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,11 +13,11 @@ def edit
 end
 
 def new
-  @post = Post.create
+  @post = Post.new
 end
 
 def create
-  @post = Post.find(params[:id])
+  @post = Post.create(params[:post])
   
   respond_to do |format|
     if @post.save
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   markdownize! :body
+  
+  def summary(lenght=300)
+    rendered_body[0..300] + "..."
+  end
 end
