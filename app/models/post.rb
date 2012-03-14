db2634cf4cb2e30419f5497f357de61c1e3214e7
Greggory Rothmeier <greggroth@gmail.com>
2012-03-14 17:00:30 -0400

    changed published_at time format

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,12 @@ class Post < ActiveRecord::Base
   validates_presence_of :title, :body
   markdownize! :body
   
-  def summary(length=300)
-    rendered_body.gsub(/(<[^>]*>)|\n|\t/s," ")[0..length] + "..."
+  def summary(max_length=300)
+    rendered_body.gsub(/(<[^>]*>)|\n|\t/s," ")[0..max_length] + "..."
   end
   
   def published_at
-    created_at.strftime("Published at %I:%M%p on %m/%d/%y")
+    created_at.strftime("Published at %l:%M%p on %m/%d/%y")
   end
   
   def tag_tokens=(ids)
